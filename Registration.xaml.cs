65de2eaaf3c88f26e7448c7bdd283c040ae63818
Prakash Selvaraj <pselvaraj@testsys.com>
2023-12-15 23:18:52 +0530

    first commit

---
+++ Registration.xaml.cs
@@ -0,0 +1,113 @@
+﻿using DeviceId;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WApp1
+{
+    /// <summary>
+    /// Interaction logic for Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        readonly string SALT = "4yJmEsPGObWvXEbcLL7qS0Lb5qe6J4";
+
+        public Registration()
+        {
+            InitializeComponent();
+            //PostAsync();
+
+            //var externalIpTask = GetExternalIpAddress();
+            //GetExternalIpAddress().Wait();
+            //var externalIpString = externalIpTask.Result ?? IPAddress.Loopback;
+            DataContext = new RegistrationViewModel();
+
+            
+            
+
+        }
+
+
+
+        private void register_Click(object sender, RoutedEventArgs e)
+        {
+            if (((RegistrationViewModel)DataContext).PostAsync().Result)
+            {
+                Close();
+            }
+        }
+
+        private void license_key_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            
+        }
+
+        private string GetMacAddress()
+        {
+            return new DeviceIdBuilder()
+                .AddMacAddress().ToString();
+        }
+
+        private string GetOSID()
+        {
+            return Environment.OSVersion.ToString();
+        }
+
+        static string GetIPAddress()
+        {
+            String address = "";
+            HttpClient httpClient = new HttpClient();
+            address = httpClient.GetStringAsync("http://checkip.dyndns.org/").Result;
+
+            int first = address.IndexOf("Address: ") + 9;
+            int last = address.LastIndexOf("</body>");
+            address = address.Substring(first, last - first);
+            return address;
+        }
+
+        private string GenerateLicenseKey()
+        {
+            string result = string.Empty;
+            if (!string.IsNullOrEmpty(macid.Text) && !string.IsNullOrEmpty(os_id.Text))
+            {
+                string data = $"{macid.Text + phone_number.Text + SALT + os_id.Text}";
+                SHA512 sha = SHA512.Create("SHA512");
+                byte[] hashData = sha.ComputeHash(Encoding.Default.GetBytes(data));
+                StringBuilder returnValue = new StringBuilder();
+                for (int i = 0; i < hashData.Length; i++)
+                {
+                    returnValue.Append(hashData[i].ToString("x2"));
+                }
+
+                result = returnValue.ToString();
+            }
+
+            return result;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            macid.Text = GetMacAddress();
+            static_ip.Text = GetIPAddress();
+            os_id.Text = GetOSID();
+            license_key.Text = GenerateLicenseKey();
+        }
+    }
+}
---
+++ RegistrationViewModel.cs
@@ -0,0 +1,134 @@
+﻿using DeviceId;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WApp1
+{
+    internal class RegistrationViewModel : IDataErrorInfo
+    {
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string CompanyName { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+        public string MacID { get; set; } = "";
+        public string LicenseKey { get; set; } = "";
+        public string StaticIP { get; set; } = "";
+        public string OSID { get; set; } = "";
+
+        public string Error { get; }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case "Email":
+                        if (string.IsNullOrEmpty(Email))
+                            return "Please enter Email.";
+                        break;
+                    case "Password":
+                        if (string.IsNullOrEmpty(Password))
+                            return "Please enter Password.";
+                        break;
+                    case "FirstName":
+                        if (string.IsNullOrEmpty(FirstName))
+                            return "Please enter FirstName.";
+                        break;
+                    case "LastName":
+                        if (string.IsNullOrEmpty(LastName))
+                            return "Please enter LastName.";
+                        break;
+                    case "CompanyName":
+                        if (string.IsNullOrEmpty(CompanyName))
+                            return "Please enter CompanyName.";
+                        break;
+                    case "PhoneNumber":
+                        if (string.IsNullOrEmpty(PhoneNumber))
+                            return "Please enter PhoneNumber.";
+                        break;
+                    case "MacID":
+                        if (string.IsNullOrEmpty(MacID))
+                            return "Please enter MacID.";
+                        break;
+                    case "LicenseKey":
+                        if (string.IsNullOrEmpty(LicenseKey))
+                            return "Please enter LicenseKey.";
+                        break;
+                    case "StaticIP":
+                        if (string.IsNullOrEmpty(StaticIP))
+                            return "Please enter StaticIP.";
+                        break;
+                    case "OSID":
+                        if (string.IsNullOrEmpty(OSID))
+                            return "Please enter OSID.";
+                        break;
+                }
+                return string.Empty;
+            }
+        }
+
+        public async Task<bool> PostAsync()
+        {
+            bool result = false;
+            //try
+            //{
+            //    HttpClient httpClient = new HttpClient();
+
+            //    using StringContent jsonContent = new(
+            //        JsonSerializer.Serialize(new
+            //        {
+            //            email = Email,
+            //            password = Password,
+            //            first_name = FirstName,
+            //            last_name = LastName,
+            //            company_name = CompanyName,
+            //            phone_number = PhoneNumber,
+            //            mac_id = MacID,
+            //            license_key = LicenseKey,
+            //            static_ip = StaticIP,
+            //            os_id = OSID
+            //        }),
+            //        Encoding.UTF8,
+            //        "application/json");
+
+            //    using HttpResponseMessage response = await httpClient.PostAsync(
+            //        "https://api.tailortrix.com/rest/api/user/register/",
+            //        jsonContent);
+            //    if (!response.IsSuccessStatusCode)
+            //    {
+            //        var errorResponse = await response.Content.ReadAsStringAsync();
+            //        MessageBox.Show(errorResponse);
+            //    }
+            //    else
+            //    {
+            //        //response.EnsureSuccessStatusCode().WriteRequestToConsole();
+
+            //        var jsonResponse = await response.Content.ReadAsStringAsync();
+            //        Console.WriteLine($"{jsonResponse}\n");
+            //        MessageBox.Show(jsonResponse);
+            //        result = true;
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message + Environment.NewLine + ex.StackTrace);
+            //}
+
+            return result;
+        }
+
+
+    }
+}
